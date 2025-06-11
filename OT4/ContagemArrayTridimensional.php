1ef50aa7424d1817d1b9cf49d90e532b31c373b4
IanDerick <ian.derick@outlook.com>
2025-06-11 20:18:24 -0300

    Adicionando mais livros dentro do array tridimensional

--- OT4/ContagemArrayTridimensional.php
+++ OT4/ContagemArrayTridimensional.php
@@ -2,11 +2,14 @@
     $colecao = [
         [// prateleira 1
             ["titulo" =>  "Dom Casmurro", "autor" => "Machado de Assis", "ano" => 1899], 
-            ["titulo" => "O Cortiço", "autor" => "Aluísio Azevedo", "ano" => 1890], 
+            ["titulo" => "O Cortiço", "autor" => "Aluísio Azevedo", "ano" => 1890],
+            ["titulo" => "Macunaíma", "autor" => "Mário de Andrade", "ano" => 1928] 
             
         ],
         [// prateleira 2
-            ["titulo" => "Memórias Póstumas", "autor" => "Machado de Assis", "ano" => 1881]
+            ["titulo" => "Memórias Póstumas", "autor" => "Machado de Assis", "ano" => 1881],
+            ["titulo" => "O Guarani", "autor" => "José de Alencar", "ano" => 1857],
+            ["titulo" => "A Moreninha", "autor" => "Joaquim Manuel de Macedo", "ano" => 1844]
         ],
         [// prateleira 3
             ["titulo" => "Iracema", "autor" => "José de Alencar", "ano" => 1865],
