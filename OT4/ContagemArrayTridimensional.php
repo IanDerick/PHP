54d98114123af41f4e71cacab73f6265402a02fc
IanDerick <ian.derick@outlook.com>
2025-06-11 20:17:20 -0300

    Fazendo um array tridimensional e contando quantos livros tem dentro desse array

---
+++ OT4/ContagemArrayTridimensional.php
@@ -0,0 +1,25 @@
+<?php
+    $colecao = [
+        [// prateleira 1
+            ["titulo" =>  "Dom Casmurro", "autor" => "Machado de Assis", "ano" => 1899], 
+            ["titulo" => "O Cortiço", "autor" => "Aluísio Azevedo", "ano" => 1890], 
+            
+        ],
+        [// prateleira 2
+            ["titulo" => "Memórias Póstumas", "autor" => "Machado de Assis", "ano" => 1881]
+        ],
+        [// prateleira 3
+            ["titulo" => "Iracema", "autor" => "José de Alencar", "ano" => 1865],
+            ["titulo" => "Capitães da Areia", "autor" => "Jorge Amado", "ano" => 1937],
+            ["titulo" => "Vidas Secas", "autor" => "Graciliano Ramos", "ano" => 1938]
+        ]
+    ];
+
+    $totalLivros = 0;
+
+
+    foreach ( $colecao as $prateleiras) {
+        $totalLivros += count($prateleiras);
+    }
+
+    echo"Numer total de livros: ". $totalLivros;
